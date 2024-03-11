360e09ae7224c0a2ef147958bb164aa7721c370c
Sanskriti <sanskritisri27@gmail.com>
2024-03-11 00:41:37 +0000

    First project code

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,30 @@
 import numpy as np
 
 def calculate(list):
+    matrix = np.array(list).reshape(3,3)
+    # calculating the mean
+    mean_axis1 = np.mean(matrix, axis = 1)
+    mean_axis2 = np.mean(matrix, axis = 0)   
+    mean_flattened = np.mean(matrix)
+     #calculating the variance
 
+     git remote set-url origin https://github.com/<github-username>/<repository-name>.git
 
 
 
     return calculations
+x = []
+y = int(input('Please enter a list of 9 numbers:'))
+if y == len(9):
+    for i in range (y):
+        ele = int(input())
+        x.append(ele)
+else:
+    print('Value Error: list must contain 9 numbers')
+
+calculations = calculate(y)
+print(calculations)
+
+
+
+    
