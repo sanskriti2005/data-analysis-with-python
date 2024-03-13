a29a0468e81d3ba5848f24fa1ca59be93f41c94b
Sanskriti <sanskritisri27@gmail.com>
2024-03-13 01:19:22 +0000

    final code, version 1

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,58 +2,57 @@ import numpy as np
 
 def calculate(list):
     matrix = np.array(list).reshape(3,3)
+
     # calculating the mean
     mean_axis1 = np.mean(matrix, axis = 1)
     mean_axis2 = np.mean(matrix, axis = 0)   
     mean_flattened = np.mean(matrix)
     
      #calculating the variance
-     var_axis1 = np.var(matrix, axis = 1)
-     var_axis2 = np.var(matrix, axis = 0)
-     var_flattened = np.var(matrix)
+    var_axis1 = np.var(matrix, axis = 1)
+    var_axis2 = np.var(matrix, axis = 0)
+    var_flattened = np.var(matrix)
 
      #CALCULATING THE STANADARD DEVIATION
-     std_axis1 = np.std(matrix, axis = 1)
-     std_axis2 = np.std(matrix, axis = 0)
-     std_flattened = np.std(matrix)
+    std_axis1 = np.std(matrix, axis = 1)
+    std_axis2 = np.std(matrix, axis = 0)
+    std_flattened = np.std(matrix)
 
      #CALCULATING THE MAX
-     max_axis1 = max(matrix, axis = 1)
-     max_axis2 = max(matrix, axis = 0)
-     max_flattened = max(matrix)
+    max_axis1 = np.max(matrix, axis = 1)
+    max_axis2 = np.max(matrix, axis = 0)
+    max_flattened = np.max(matrix)
 
      #CALCULATING THE MIN
-     min_axis1 = min(matrix, axis = 1)
-     min_axis2 = min(matrix, axis = 0)
-     min_flattened = min(matrix)
+    min_axis1 = np.min(matrix, axis = 1)
+    min_axis2 = np.min(matrix, axis = 0)
+    min_flattened = np.min(matrix)
 
      #CALCULATING THE SUM 
-     sum_axis1 = sum(matrix, axis = 1)
-     sum_axis2 = sum(matrix, axis = 0)
-     sum_flattened = sum(matrix)
+    sum_axis1 = np.sum(matrix, axis = 1)
+    sum_axis2 = np.sum(matrix, axis = 0)
+    sum_flattened = np.sum(matrix)
 
 
-    calculations = { 'mean': [mean_axis1, mean_axis2, mean_flattened],
+    calculations = { 'mean': [mean_axis1, mean_axis2, mean_flattened], 
         'variance': [var_axis1, var_axis2, var_flattened],
-        'standard_deviation': [std_axis1, std_axis2, std_flattened],
-        'max': [max_axis1, max_axis2, max_flattened],
-        'min': [min_axis1, min_axis2, min_flattened],
+        'standard_deviation': [std_axis1, std_axis2, std_flattened], 
+        'max': [max_axis1, max_axis2, max_flattened], 
+        'min': [min_axis1, min_axis2, min_flattened], 
         'sum': [sum_axis1, sum_axis2, sum_flattened] }
 
 
-
     return calculations
-x = []
-y = int(input('Please enter a list of 9 numbers:'))
-if y == len(9):
-    for i in range (y):
-        ele = int(input())
-        x.append(ele)
+
+
+user_input = input('Please enter a list of 9 numbers(separated by space):')
+number_list = list(user_input.split())
+int_list = [int(num) for num in number_list]
+if len(int_list) == 9:
+    calculations = calculate(int_list)
+    print(calculations)
 else:
     print('Value Error: list must contain 9 numbers')
-
-calculations = calculate(y)
-print(calculations)
 
 
 
