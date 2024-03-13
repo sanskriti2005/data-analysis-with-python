89c3a440a2880a37411ceda6f7c09dfb1372d75a
Sanskriti <sanskritisri27@gmail.com>
2024-03-13 00:12:24 +0000

    Wrote the code for calculating the variance, standard deviation, minimum and maximum

--- mean_var_std.py
+++ mean_var_std.py
@@ -6,7 +6,27 @@ def calculate(list):
     mean_axis1 = np.mean(matrix, axis = 1)
     mean_axis2 = np.mean(matrix, axis = 0)   
     mean_flattened = np.mean(matrix)
+    
      #calculating the variance
+     var_axis1 = np.var(matrix, axis = 1)
+     var_axis2 = np.var(matrix, axis = 0)
+     var_flattened = np.var(matrix)
+
+     #CALCULATING THE STANADARD DEVIATION
+     std_axis1 = np.std(matrix, axis = 1)
+     std_axis2 = np.std(matrix, axis = 0)
+     std_flattened = np.std(matrix)
+
+     #CALCULATING THE MAX
+     max_axis1 = max(matrix, axis = 1)
+     max_axis2 = max(matrix, axis = 0)
+     max_flattened = max(matrix)
+
+     #CALCULATING THE MIN
+     min_axis1 = min(matrix, axis = 1)
+     min_axis2 = min(matrix, axis = 0)
+     min_flattened = min(matrix)
+     
 
      
 
