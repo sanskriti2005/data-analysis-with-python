549a54bbbd674dca9e86bbe4d641c7f5d3cc25e7
Sanskriti <sanskritisri27@gmail.com>
2024-03-27 08:46:26 +0000

    passed

--- mean_var_std.py
+++ mean_var_std.py
@@ -5,42 +5,42 @@ def calculate(list):
         matrix = np.array(list).reshape(3, 3)
 
         # Calculating the mean
-        mean_axis1 = np.mean(matrix, axis=1)
-        mean_axis2 = np.mean(matrix, axis=0)
+        mean_axis1 = np.mean(matrix, axis=1).tolist()
+        mean_axis2 = np.mean(matrix, axis=0).tolist()
         mean_flattened = np.mean(matrix)
 
         # Calculating the variance
-        var_axis1 = np.var(matrix, axis=1)
-        var_axis2 = np.var(matrix, axis=0)
+        var_axis1 = np.var(matrix, axis=1).tolist()
+        var_axis2 = np.var(matrix, axis=0).tolist()
         var_flattened = np.var(matrix)
 
         # Calculating the standard deviation
-        std_axis1 = np.std(matrix, axis=1)
-        std_axis2 = np.std(matrix, axis=0)
+        std_axis1 = np.std(matrix, axis=1).tolist()
+        std_axis2 = np.std(matrix, axis=0).tolist()
         std_flattened = np.std(matrix)
 
         # Calculating the max
-        max_axis1 = np.max(matrix, axis=1)
-        max_axis2 = np.max(matrix, axis=0)
+        max_axis1 = np.max(matrix, axis=1).tolist()
+        max_axis2 = np.max(matrix, axis=0).tolist()
         max_flattened = np.max(matrix)
 
         # Calculating the min
-        min_axis1 = np.min(matrix, axis=1)
-        min_axis2 = np.min(matrix, axis=0)
+        min_axis1 = np.min(matrix, axis=1).tolist()
+        min_axis2 = np.min(matrix, axis=0).tolist()
         min_flattened = np.min(matrix)
 
         # Calculating the sum
-        sum_axis1 = np.sum(matrix, axis=1)
-        sum_axis2 = np.sum(matrix, axis=0)
+        sum_axis1 = np.sum(matrix, axis=1).tolist()
+        sum_axis2 = np.sum(matrix, axis=0).tolist()
         sum_flattened = np.sum(matrix)
 
         calculations = {
-            'mean': [mean_axis1, mean_axis2, mean_flattened],
-            'variance': [var_axis1, var_axis2, var_flattened],
-            'standard_deviation': [std_axis1, std_axis2, std_flattened],
-            'max': [max_axis1, max_axis2, max_flattened],
-            'min': [min_axis1, min_axis2, min_flattened],
-            'sum': [sum_axis1, sum_axis2, sum_flattened]
+            'mean': [mean_axis2, mean_axis1, mean_flattened],
+            'variance': [var_axis2, var_axis1, var_flattened],
+            'standard_deviation': [std_axis2, std_axis1, std_flattened],
+            'max': [max_axis2, max_axis1, max_flattened],
+            'min': [min_axis2, min_axis1, min_flattened],
+            'sum': [sum_axis2, sum_axis1, sum_flattened]
         }
 
         return calculations
