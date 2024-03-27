71369065e6bab58a9a6e7df004478c81cf26998a
Sanskriti <sanskritisri27@gmail.com>
2024-03-27 08:10:40 +0000

    passed value error test

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,50 +1,54 @@
 import numpy as np
 
-def calculator(list):
-    if list != 9:
+def calculate(list):
+    if len(list) == 9:
+        matrix = np.array(list).reshape(3, 3)
 
-    matrix = np.array(list).reshape(3,3)
+        # Calculating the mean
+        mean_axis1 = np.mean(matrix, axis=1)
+        mean_axis2 = np.mean(matrix, axis=0)
+        mean_flattened = np.mean(matrix)
 
-    # calculating the mean
-    mean_axis1 = np.mean(matrix, axis = 1)
-    mean_axis2 = np.mean(matrix, axis = 0)   
-    mean_flattened = np.mean(matrix)
+        # Calculating the variance
+        var_axis1 = np.var(matrix, axis=1)
+        var_axis2 = np.var(matrix, axis=0)
+        var_flattened = np.var(matrix)
+
+        # Calculating the standard deviation
+        std_axis1 = np.std(matrix, axis=1)
+        std_axis2 = np.std(matrix, axis=0)
+        std_flattened = np.std(matrix)
+
+        # Calculating the max
+        max_axis1 = np.max(matrix, axis=1)
+        max_axis2 = np.max(matrix, axis=0)
+        max_flattened = np.max(matrix)
+
+        # Calculating the min
+        min_axis1 = np.min(matrix, axis=1)
+        min_axis2 = np.min(matrix, axis=0)
+        min_flattened = np.min(matrix)
+
+        # Calculating the sum
+        sum_axis1 = np.sum(matrix, axis=1)
+        sum_axis2 = np.sum(matrix, axis=0)
+        sum_flattened = np.sum(matrix)
+
+        calculations = {
+            'mean': [mean_axis1, mean_axis2, mean_flattened],
+            'variance': [var_axis1, var_axis2, var_flattened],
+            'standard_deviation': [std_axis1, std_axis2, std_flattened],
+            'max': [max_axis1, max_axis2, max_flattened],
+            'min': [min_axis1, min_axis2, min_flattened],
+            'sum': [sum_axis1, sum_axis2, sum_flattened]
+        }
+
+        return calculations
+    else:
+        raise ValueError("List must contain nine numbers.")
+
     
-     #calculating the variance
-    var_axis1 = np.var(matrix, axis = 1)
-    var_axis2 = np.var(matrix, axis = 0)
-    var_flattened = np.var(matrix)
 
-     #CALCULATING THE STANADARD DEVIATION
-    std_axis1 = np.std(matrix, axis = 1)
-    std_axis2 = np.std(matrix, axis = 0)
-    std_flattened = np.std(matrix)
-
-     #CALCULATING THE MAX
-    max_axis1 = np.max(matrix, axis = 1)
-    max_axis2 = np.max(matrix, axis = 0)
-    max_flattened = np.max(matrix)
-
-     #CALCULATING THE MIN
-    min_axis1 = np.min(matrix, axis = 1)
-    min_axis2 = np.min(matrix, axis = 0)
-    min_flattened = np.min(matrix)
-
-     #CALCULATING THE SUM 
-    sum_axis1 = np.sum(matrix, axis = 1)
-    sum_axis2 = np.sum(matrix, axis = 0)
-    sum_flattened = np.sum(matrix)
-
-
-    calculations = { 'mean': [mean_axis1, mean_axis2, mean_flattened], 
-        'variance': [var_axis1, var_axis2, var_flattened],
-        'standard_deviation': [std_axis1, std_axis2, std_flattened], 
-        'max': [max_axis1, max_axis2, max_flattened], 
-        'min': [min_axis1, min_axis2, min_flattened], 
-        'sum': [sum_axis1, sum_axis2, sum_flattened] }
-
-
-    return calculations
 
 
 
